a51932ae268a6e632349ae458a31edfd73063014
J-create-bit <jfr.nasa@gmail.com>
2026-09-01 22:40:50 -0300

    Cambio estructural. Se borra la ccarpeta views

--- public/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-export const supabase = createClient('https://luvyjiasmpevwbfbyizt.supabase.co', 'sb_publishable_BYnTBtPtVcu4IAZevptzAw_zYmbWiYk')
